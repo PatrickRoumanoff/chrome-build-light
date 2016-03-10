5d502d816b30b8b73f3aa86797aec54423544c52
Patrick Roumanoff <patrick.roumanoff@iag.com>
2016-03-11 09:24:36 +1100

    working zip file

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const zip = require('gulp-zip');
  
 gulp.task('default', () => {
-	return gulp.src('src/*')
+	return gulp.src(['src/**', '!src/assets/*.jpg', '!src/assets/screenshot*'])
 		.pipe(zip('delcom-build-light.zip'))
 		.pipe(gulp.dest('dist'));
 });
