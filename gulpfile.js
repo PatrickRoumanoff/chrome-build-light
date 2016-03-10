9a44bd6df3c43ba5350b9585e8e4d41f3ebedece
Patrick Roumanoff <patrick.roumanoff@iag.com>
2016-03-11 09:04:32 +1100

    reorg

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+ 
+gulp.task('default', () => {
+	return gulp.src('src/*')
+		.pipe(zip('delcom-build-light.zip'))
+		.pipe(gulp.dest('dist'));
+});
