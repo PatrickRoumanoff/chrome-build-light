b20814f2f28fa5c82af84c235170ccf3c1d74447
Patrick Roumanoff <patrick.roumanoff@iag.com>
2016-03-14 23:01:54 +1100

    radio tabs and colors

--- src/background.js
+++ src/background.js
@@ -1,10 +1,10 @@
 chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('index.html', {
         innerBounds: {
-            width: 800,
-            height: 300,
-            minWidth: 800,
-            minHeight: 300
+            width: 1100,
+            height: 450,
+            minWidth: 1100,
+            minHeight: 450
         },
         id: "DelcomLight"
     });
